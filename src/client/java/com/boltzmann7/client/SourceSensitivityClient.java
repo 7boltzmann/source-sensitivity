386b28d4c8b4eecd093b957a5a7bb73070587849
7boltzmann <317158272+7boltzmann@users.noreply.github.com>
2026-08-17 14:50:03 -0400

    - fixed contact information - cleaned up conversion code - added mathematically correct limits

--- src/client/java/com/boltzmann7/client/SourceSensitivityClient.java
+++ src/client/java/com/boltzmann7/client/SourceSensitivityClient.java
@@ -5,15 +5,16 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.ChatComponent;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.util.Mth;
 
 public class SourceSensitivityClient implements ClientModInitializer {
 
 	double convertSourceSensitivityToMinecraft(double sensitivity) {
-		double x = (1.0 / 0.6) * ((Math.pow((0.022 * sensitivity) / (0.15 * 8.0), 1.0 / 3.0)) - 0.2);
-		x = Mth.clamp(x, 0, 1);
-		return x;
+        return Mth.clamp((1.0 / 0.6) * ((Math.pow((0.022 * sensitivity) / (0.15 * 8.0), 1.0 / 3.0)) - 0.2), 0, 1);
 	}
 
 	@Override
@@ -25,19 +26,20 @@ public class SourceSensitivityClient implements ClientModInitializer {
 							.then(
 									ClientCommands.argument(
 													"sourceSensitivity",
-													DoubleArgumentType.doubleArg(0.0, 30)
+													DoubleArgumentType.doubleArg(0.43636, 27.92727) // comes from intersection of y = 0 and y = 1
 											)
 											.executes(context -> {
+												LocalPlayer player = context.getSource().getPlayer();
+
 												double sourceSensitivity = DoubleArgumentType.getDouble(context, "sourceSensitivity");
 												double minecraftSensitivity = convertSourceSensitivityToMinecraft(sourceSensitivity);
+
 												Minecraft.getInstance().options.sensitivity().set(minecraftSensitivity);
-												context.getSource()
-														.getPlayer()
-														.sendOverlayMessage(
-																Component.literal(
-																		"Sensitivity Set to " + sourceSensitivity + " (" + Math.round(minecraftSensitivity * 200) + "%)"
-																)
-														);
+												player.sendOverlayMessage(
+														Component.literal(
+																String.format("Sensitivity Set to %f (%.3f%%)", sourceSensitivity, minecraftSensitivity * 200)
+														)
+												);
 												return 1;
 											})
 							)
